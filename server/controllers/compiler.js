6e4ff8c450bd1133bf9d2a8a91373e4ac4622d35
= <=>
2022-08-31 09:25:14 +0800

    [Feat] complete compiler

--- server/controllers/compiler.js
+++ server/controllers/compiler.js
@@ -10,14 +10,13 @@ const runCompiler = async (req, res) => {
         if (error) {
           console.log(`error: ${error.message}`);
           reject(error);
-          // return;
         }
         if (stderr) {
           console.log(`stderr: ${stderr}`);
           reject(stderr);
-          // return;
         }
         if (stdout) {
+          console.log(`stdout: ${stdout}`);
           resolve(stdout);
         }
       });
@@ -25,11 +24,9 @@ const runCompiler = async (req, res) => {
   }
   const userCodeRoute = `./user_tmp_codes/${userID}.js`;
   fs.writeFileSync(userCodeRoute, codes);
-  // docker run -it -v ${pwd}:/app node-tool
-  // docker run -it -e CODE_FILE=/app/user_tmp_codes/1.js -v $(pwd)/user_tmp_codes:/app/user_tmp_codes node-tool
-  // const compilerResult = await runCommand('ls -al');
   const compilerResult = await runCommand(`docker run -e CODE_FILE=/app/user_tmp_codes/${userID}.js -v \$\(pwd\)/user_tmp_codes:/app/user_tmp_codes --rm node-tool`);
-  res.status(200).json(compilerResult);
+  fs.rmSync(userCodeRoute);
+  return res.status(200).json(compilerResult);
 };
 
 module.exports = { runCompiler };
