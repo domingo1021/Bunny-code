205ee0f107eb524e60b9909c1b42bd540f7f8f54
= <=>
2022-08-31 08:35:08 +0800

    [Fix] fix correct volume to docker container

--- server/controllers/compiler.js
+++ server/controllers/compiler.js
@@ -21,7 +21,8 @@ const runCompiler = async (req, res) => {
   }
   const userCodeRoute = `./user_tmp_codes/${userID}.js`;
   fs.writeFileSync(userCodeRoute, codes);
-  const compilerResult = await runCommand(`docker run -e CODE_FILE=${userID}.js -v \$\{pwd\}/user_tmp_codes/${userID}.js:/app --rm node-log-tool`);
+  // docker run -it -v ${pwd}:/app node-tool
+  const compilerResult = await runCommand(`docker run -e CODE_FILE=${userID}.js -v \$\{pwd\}/user_tmp_codes:/app --rm node-log-tool`);
   res.status(200).json(compilerResult);
 };
 
