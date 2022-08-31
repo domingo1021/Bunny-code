d9287cab58d4e71786b38093db2e3423447db501
= <=>
2022-08-31 08:51:16 +0800

    [Feat] test docker run

--- server/controllers/compiler.js
+++ server/controllers/compiler.js
@@ -22,7 +22,8 @@ const runCompiler = async (req, res) => {
   const userCodeRoute = `./user_tmp_codes/${userID}.js`;
   fs.writeFileSync(userCodeRoute, codes);
   // docker run -it -v ${pwd}:/app node-tool
-  const compilerResult = await runCommand(`docker run -e CODE_FILE=${userID}.js -v \$\{pwd\}/user_tmp_codes:/app --rm node-log-tool`);
+  // docker run -it -e CODE_FILE=/app/user_tmp_codes/1.js -v $(pwd)/user_tmp_codes:/app/user_tmp_codes node-tool
+  const compilerResult = await runCommand(`docker run -e CODE_FILE=/app/user_tmp_codes/${userID}.js -v \$\(pwd\)/user_tmp_codes:/app/user_tmp_codes --rm node-log-tool`);
   res.status(200).json(compilerResult);
 };
 
