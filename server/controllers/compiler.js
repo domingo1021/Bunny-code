0adf22ff165aeea9cdc783bda77c77ff5e2c185c
= <=>
2022-08-31 09:09:14 +0800

    [Feat] add promise for process result

--- server/controllers/compiler.js
+++ server/controllers/compiler.js
@@ -5,25 +5,30 @@ const runCompiler = async (req, res) => {
   const { userID } = req.body;
   const { codes } = req.body;
   async function runCommand(cmd) {
-    let result;
-    exec(cmd, (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-      }
-      result = stdout;
+    return new Promise((resolve, reject) => {
+      exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+          console.log(`error: ${error.message}`);
+          reject(error);
+          // return;
+        }
+        if (stderr) {
+          console.log(`stderr: ${stderr}`);
+          reject(stderr);
+          // return;
+        }
+        if (stdout) {
+          resolve(stdout);
+        }
+      });
     });
-    return result;
   }
   const userCodeRoute = `./user_tmp_codes/${userID}.js`;
   fs.writeFileSync(userCodeRoute, codes);
   // docker run -it -v ${pwd}:/app node-tool
   // docker run -it -e CODE_FILE=/app/user_tmp_codes/1.js -v $(pwd)/user_tmp_codes:/app/user_tmp_codes node-tool
-  const compilerResult = await runCommand(`docker run -e CODE_FILE=/app/user_tmp_codes/${userID}.js -v \$\(pwd\)/user_tmp_codes:/app/user_tmp_codes --rm node-log-tool`);
+  // const compilerResult = await runCommand('ls -al');
+  const compilerResult = await runCommand(`docker run -e CODE_FILE=/app/user_tmp_codes/${userID}.js -v \$\(pwd\)/user_tmp_codes:/app/user_tmp_codes --rm node-tool`);
   res.status(200).json(compilerResult);
 };
 
