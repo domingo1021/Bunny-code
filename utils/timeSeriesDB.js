d4db4c0d36e246278d5043323ccf8d4761141a70
= <=>
2022-09-01 11:27:33 +0800

    [Feat] add write api with self defined timestamp

--- utils/timeSeriesDB.js
+++ utils/timeSeriesDB.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { InfluxDB } = require('@influxdata/influxdb-client');
+const { InfluxDB, WritePrecision, Point } = require('@influxdata/influxdb-client');
 
 // You can generate an API token from the "API Tokens Tab" in the UI
 const {
@@ -10,13 +10,14 @@ console.log(INFLUX_URL, INFLUX_TOKEN, INFLUX_ORG, INFLUX_BUCKET);
 
 const client = new InfluxDB({ url: INFLUX_URL, token: INFLUX_TOKEN });
 
-// Write data
-const { Point } = require('@influxdata/influxdb-client');
+// Write data, point in as single data record
+// In a series, each point has a unique timestamp.
 
-const writeApi = client.getWriteApi(INFLUX_ORG, INFLUX_BUCKET);
+const writeApi = client.getWriteApi(INFLUX_ORG, INFLUX_BUCKET, 'ns');
 writeApi.useDefaultTags({ host: 'host1' });
 
-const point = new Point('mem').floatField('used_percent', 23.43234543);
+// 使用 Point 製造 InfluxDB line protocal，並寫入 with timestamp， 記得用 'ns'
+const point = new Point('mem').tag('host_id', '12345').floatField('used_percent', 666).timestamp(`${new Date().getTime()}000000`);
 writeApi.writePoint(point);
 
 writeApi
@@ -32,11 +33,12 @@ writeApi
 // Flux query
 const queryApi = client.getQueryApi(INFLUX_ORG);
 
-const query = 'from(bucket: "bunny") |> range(start: -1h)';
+const query = 'from(bucket: "bunny") |> range(start: 0)';
 queryApi.queryRows(query, {
   next(row, tableMeta) {
     const o = tableMeta.toObject(row);
-    console.log(`${o._time} ${o._measurement}: ${o._field}=${o._value}`);
+    // console.log(o);
+    console.log(`${o._time} ${o._measurement}: host=${o.host} ${o._field}=${o._value}`);
   },
   error(error) {
     console.error(error);
