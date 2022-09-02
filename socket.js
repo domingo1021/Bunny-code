de3bab01b3de72751d308399054bd29f98a5b731
= <=>
2022-09-02 16:20:13 +0800

    [Feat] write user keyboard data into influx db

--- socket.js
+++ socket.js
@@ -1,5 +1,6 @@
 const { Server } = require('socket.io');
 const httpServer = require('./app');
+const { writeRecord, queryRecord } = require('./socket/codeRecord');
 
 const io = new Server(httpServer, {
   cors: {
@@ -10,9 +11,20 @@ const io = new Server(httpServer, {
   },
 });
 
-io.on('connection', (socket) => {
+// TODO: socket auth
+
+io.on('connection', async (socket) => {
   console.log('a user connected');
-  socket.on('save', (codes) => {
-    console.log(codes);
+  socket.on('save', async (codes) => {
+    const userID = 1;
+    const projectID = 1;
+    const responseMsg = await writeRecord(userID, projectID, JSON.parse(codes));
+    socket.emit('return', responseMsg);
+    // const records = await queryRecord(1, 1, '2022-09-02T04:25:32.985Z', '2022-09-03T04:25:32.47Z');
+    // console.log(records);
   });
+  const start = `${new Date('2022-09-02T04:25:32.985Z').getTime()}000000}`;
+  const end = `${new Date('2022-09-03T04:25:32.47Z').getTime()}000000}`;
+  const records = await queryRecord('1', '1', start, end);
+  console.log(records);
 });
--- test/InfluxDB_api.js
+++ test/InfluxDB_api.js
@@ -9,10 +9,12 @@ const { INFLUX_ORG, INFLUX_BUCKET } = process.env;
 
 const writeApi = influxClient.getWriteApi(INFLUX_ORG, INFLUX_BUCKET, 'ns');
 writeApi.useDefaultTags({ host: 'host1' });
+writeApi.writeRecord(`user,project=1,action="create",line=1,index=2 code="e" ${new Date().getTime()}000000`);
 
-// 使用 Point 製造 InfluxDB line protocal，並寫入 with timestamp， 記得用 'ns'
+// // 使用 Point 製造 InfluxDB line protocal，並寫入 with timestamp， 記得用 'ns'
 const point = new Point('mem').tag('host_id', '12345').floatField('used_percent', 666).timestamp(`${new Date().getTime()}000000`);
-writeApi.writePoint(point);
+const point2 = new Point('men').tag('host_id', '12345').floatField('used_12345', 666).timestamp(`${new Date().getTime()}000000`);
+writeApi.writePoints([point, point2]);
 
 writeApi
   .close()
@@ -27,12 +29,24 @@ writeApi
 // Flux query
 const queryApi = influxClient.getQueryApi(INFLUX_ORG);
 
-// filter
-const query = 'from(bucket: "bunny") |> range(start: 0)';
+// // filter
+const query = `from(bucket: "bunny")
+                |> range(start: 0)
+                |> filter(fn: (r) => r["_measurement"] == "1")
+              `;
+// _measurement: userID
+// project: projectID
+// action: create/delete/...
+// index: keyword index
+// _field: code
+// _value: inputed code
+// _time: '2022-09-02T04:25:32.47Z',
+
 queryApi.queryRows(query, {
   next(row, tableMeta) {
     const o = tableMeta.toObject(row);
-    console.log(`${o._time} ${o._measurement}: host=${o.host} ${o._field}=${o._value}`);
+    console.log(o);
+    console.log(`${o._time} ${o._measurement}: host=${o.project} ${o._field}=${o._value}`);
   },
   error(error) {
     console.error(error);
