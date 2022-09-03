2ce5aa0583536797e7e99666dff7cfb7de5a25ce
= <=>
2022-09-03 10:59:01 +0800

    [Feat] complete main function table schema

--- schema.js
+++ schema.js
@@ -4,23 +4,24 @@ const pool = require('./utils/rmdb');
 
 const userTable = `
   CREATE TABLE IF NOT EXISTS user(
-    user_id INT NOT NULL UNSIGNED AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     user_name VARCHAR(30) NOT NULL,
     email NVARCHAR(255) NOT NULL,
     password BINARY(60) NOT NULL,
-    follower_count INT NOT NULL UNSIGNED DEFAULT 0,
+    follower_count INT UNSIGNED NOT NULL DEFAULT 0,
     profile VARCHAR(100) NOT NULL,
     picture VARCHAR(100),
-    UNIQUE (Email),
+    UNIQUE (user_name),
+    UNIQUE (email),
     PRIMARY KEY (user_id)
   );
 `;
 
 const relationshipTable = `
   CREATE TABLE IF NOT EXISTS relationship(
-    relation_id INT NOT NULL UNSIGNED AUTO_INCREMENT,
-    master_id INT NOT NULL UNSIGNED,
-    follower_id INT NOT NULL UNSIGNED,
+    relationship_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    master_id INT UNSIGNED NOT NULL,
+    follower_id INT UNSIGNED NOT NULL,
     FOREIGN KEY (master_id) REFERENCES user(user_id) ON DELETE CASCADE,
     FOREIGN KEY (follower_id) REFERENCES user(user_id) ON DELETE CASCADE,
     PRIMARY KEY (relationship_id)
@@ -29,23 +30,26 @@ const relationshipTable = `
 
 const projectTable = `
   CREATE TABLE IF NOT EXISTS project(
-    project_id INT NOT NULL UNSIGNED AUTO_INCREMENT,
+    project_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     project_name VARCHAR(30) NOT NULL,
-    watch_count INT NOT NULL UNSIGNED DEFAULT 0,
-    star_count INT NOT NULL UNSIGNED DEFAULT 0,
+    watch_count INT UNSIGNED NOT NULL DEFAULT 0,
+    star_count INT UNSIGNED NOT NULL DEFAULT 0,
     is_public TINYINT(1) NOT NULL,
-    user_id INT NOT NULL UNSIGNED,
-    FOREIGN KEY (follower_id) REFERENCES user(user_id) ON DELETE SET NULL,
+    user_id INT UNSIGNED NOT NULL,
+    deleted TINYINT(1) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user(user_id),
+    UNIQUE (project_name),
     PRIMARY KEY (project_id)
   );
 `;
 
 const versionTable = `
   CREATE TABLE IF NOT EXISTS version(
-    version_id INT NOT NULL UNSIGNED AUTO_INCREMENT,
+    version_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     version_name VARCHAR(30) NOT NULL,
-    project_id INT NOT NULL UNSIGNED,
-    FOREIGN KEY (project_id) REFERENCES project(project_id) ON DELETE SET NULL,
+    project_id INT UNSIGNED NOT NULL,
+    deleted TINYINT(1) NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES project(project_id),
     PRIMARY KEY (version_id)
   );
 `;
@@ -53,35 +57,64 @@ const versionTable = `
 // s3 storage.
 const fileTable = `
   CREATE TABLE IF NOT EXISTS file(
-    file_id INT NOT NULL UNSIGNED AUTO_INCREMENT,
+    file_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     file_name VARCHAR(30) NOT NULL,
     file_url VARCHAR(100) NOT NULL,
-    version_id INT NOT NULL UNSIGNED,
-    FOREIGN KEY (version_id) REFERENCES version(version_id) ON DELETE SET NULL,
+    version_id INT UNSIGNED NOT NULL,
+    deleted TINYINT(1) NOT NULL,
+    FOREIGN KEY (version_id) REFERENCES version(version_id),
     PRIMARY KEY (file_id)
   );
 `;
 
 const recordTable = `
   CREATE TABLE IF NOT EXISTS record(
-    record_id INT NOT NULL UNSIGNED AUTO_INCREMENT,
+    record_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
     start_time timestamp NOT NULL,
     end_time timestamp NOT NULL,
-    version_id INT NOT NULL UNSIGNED,
-    FOREIGN KEY (version_id) REFERENCES version(version_id) ON DELETE SET NULL,
-    PRIMARY KEY record_id)
+    version_id INT UNSIGNED NOT NULL,
+    deleted TINYINT(1) NOT NULL,
+    FOREIGN KEY (version_id) REFERENCES version(version_id),
+    PRIMARY KEY (record_id)
   );
 `;
 
 const streamTable = `
-
+  CREATE TABLE IF NOT EXISTS stream(
+    stream_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    viewer_counts INT UNSIGNED NOT NULL DEFAULT 0,
+    project_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (project_id) REFERENCES project(project_id),
+    PRIMARY KEY (stream_id)
+  );
 `;
 
-const chatroomTable = `
+const messageTable = `
+  CREATE TABLE IF NOT EXISTS chat_room(
+    message_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    message_time DATETIME NOT NULL,
+    stream_id INT UNSIGNED NOT NULL,
+    FOREIGN KEY (stream_id) REFERENCES stream(stream_id),
+    PRIMARY KEY (message_id)
+  );
 `;
 
 const battleTable = `
-
+  CREATE TABLE IF NOT EXISTS battle(
+    battle_id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    battle_name VARCHAR(30) NOT NULL,
+    watch_count INT UNSIGNED NOT NULL DEFAULT 0,
+    star_count INT UNSIGNED NOT NULL DEFAULT 0,
+    first_user_id INT UNSIGNED NOT NULL,
+    second_user_id INT UNSIGNED NOT NULL,
+    is_public TINYINT(1) NOT NULL,
+    winner_id INT,
+    deleted TINYINT(1) NOT NULL,
+    FOREIGN KEY (first_user_id) REFERENCES user(user_id),
+    FOREIGN KEY (second_user_id) REFERENCES user(user_id),
+    UNIQUE (battle_name),
+    PRIMARY KEY (battle_id)
+  );
 `;
 
 const userRoleTable = `
@@ -95,4 +128,33 @@ const rolePermissionTable = `
 `;
 
 const permissionTable = `
+
 `;
+
+pool.execute(userTable).then(() => console.log('success')).catch((error) => {
+  console.log(error);
+});
+pool.execute(relationshipTable).then(() => console.log('success')).catch((error) => {
+  console.log(error);
+});
+pool.execute(projectTable).then(() => console.log('success')).catch((error) => {
+  console.log(error);
+});
+pool.execute(versionTable).then(() => console.log('success')).catch((error) => {
+  console.log(error);
+});
+pool.execute(fileTable).then(() => console.log('success')).catch((error) => {
+  console.log(error);
+});
+pool.execute(recordTable).then(() => console.log('success')).catch((error) => {
+  console.log(error);
+});
+pool.execute(streamTable).then(() => console.log('success')).catch((error) => {
+  console.log(error);
+});
+pool.execute(messageTable).then(() => console.log('success')).catch((error) => {
+  console.log(error);
+});
+pool.execute(battleTable).then(() => console.log('success')).catch((error) => {
+  console.log(error);
+});
