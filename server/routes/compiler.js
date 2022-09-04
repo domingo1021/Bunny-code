5d51c95adf64528ab103a36a725264907d8faf7a
= <=>
2022-09-04 13:24:04 +0800

    [Feat] test upload function

--- server/routes/compiler.js
+++ server/routes/compiler.js
@@ -1,8 +1,13 @@
 const router = require('express').Router();
+const { fileUploader } = require('../services/service');
+const { jwtAuthenticate } = require('../services/auth');
 const { runCompiler, writeRecord, queryRecord } = require('../controllers/compiler');
+const uploadS3 = require('../services/cloudStorage');
 
 router.get('/compiler', runCompiler);
 
 router.route('/record').get(queryRecord).post(writeRecord);
+
+router.post('/record/file', jwtAuthenticate, fileUploader, uploadS3, (req, res) => res.send('hello world !'));
 
 module.exports = router;
