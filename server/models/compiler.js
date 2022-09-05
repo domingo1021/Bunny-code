988e380dd70ac2f3c4ba7bf85701cf39fe07373e
= <=>
2022-09-05 17:49:25 +0800

    [Fix] fix typo for end time

--- server/models/compiler.js
+++ server/models/compiler.js
@@ -7,7 +7,7 @@ const writeFile = async (fileName, fileURL, log, versionID) => {
 
 const writeRecord = async (versionID, startTime, endTime) => {
   const sql = 'INSERT INTO record (version_id, start_time, end_time) VALUES (?, ?, ?);';
-  await pool.execute(sql, [versionID, startTime.endTime]);
+  await pool.execute(sql, [versionID, startTime, endTime]);
 };
 
 module.exports = { writeFile, writeRecord };
