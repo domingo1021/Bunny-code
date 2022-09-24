81adfd3996767c8e2a33446acb784287455f79b5
= <=>
2022-09-24 22:31:41 +0800

    [Feat] remove redundant console

--- docker_tool/twoSum.js
+++ docker_tool/twoSum.js
@@ -1,9 +1,6 @@
 // require('dotenv').config()
 const { twoSum } = require(process.env.TWO_SUM_FILE);
 
-console.log(process.env.TWOSUMFILE);
-console.log(twoSum);
-
 const compilerResults = [];
 compilerResults.push(twoSum([2, 7, 11, 15], 9));
 compilerResults.push(twoSum([2, 7, 11, 15], 26));
