f666d11ee0a99d7d9a328ef21ed96ac7dcd36062
= <=>
2022-09-24 22:41:49 +0800

    [Feat] add two sum test case

--- docker_tool/twoSum.js
+++ docker_tool/twoSum.js
@@ -6,5 +6,6 @@ compilerResults.push(twoSum([2, 7, 11, 15], 9));
 compilerResults.push(twoSum([2, 7, 11, 15], 26));
 compilerResults.push(twoSum([1, 1, 1, 1], 2));
 compilerResults.push(twoSum([-1], 10));
+compilerResults.push(twoSum([1314, 2234, 1234, 4243], 3468));
 
 console.log(compilerResults);
