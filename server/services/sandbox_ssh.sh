5b056a50c17f87295b9e74d42200ad73b71820a8
= <=>
2022-10-12 23:26:42 +0800

    [Feat] add ssh arange sandbox jobs shell script

--- server/services/sandbox_ssh.sh
+++ server/services/sandbox_ssh.sh
@@ -9,15 +9,15 @@ do
         esac
 done
 
-scp -q -i ${identity_file} ./${file_name} ubuntu@${host_name}:~/test_slave/${file_name}
+scp -q -i ${identity_file} ./${file_name} ubuntu@${host_name}:~/sandbox_jobs/${file_name}
 ssh -i ${identity_file} ubuntu@${host_name} "bash -s ${file_name} ${target_dir} ${container_name}" << 'EOF'
 #!/bin/bash
 file_name=$1
 target_dir=$2
 container_name=$3
-cd ~/test_slave
+cd ~/sandbox_jobs
 docker run --cpus="0.1" --memory=20m -v $(pwd)/${file_name}:/bunny_code/${target_dir}/${file_name} --name ${container_name} node-tool /bunny_code/${target_dir}/${file_name}
 docker container inspect ${container_name} -f '{"OOM": {{json .State.OOMKilled}}}'
 docker rm ${container_name} >/dev/null 2>&1
-rm -f ~/test_slave/${file_name}
+rm -f ~/sandbox_jobs/${file_name}
 EOF
