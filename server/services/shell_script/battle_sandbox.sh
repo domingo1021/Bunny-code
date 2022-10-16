757882b5f42d099a7c815667775f80d09f72dc4d
= <=>
2022-10-16 15:54:46 +0800

    [Fix] fix battl shell script typo

--- server/services/shell_script/battle_sandbox.sh
+++ server/services/shell_script/battle_sandbox.sh
@@ -2,7 +2,7 @@ while getopts "f:d:c:h:i:e:s:" flag
 do
         case "${flag}" in
                 f) file_name=${OPTARG};;
-                d) target_dir=${OPTARG};;
+                d) file_dir=${OPTARG};;
                 e) test_case_env=${OPTARG};;
                 s) test_case_script=${OPTARG};;
                 c) container_name=${OPTARG};;
@@ -19,7 +19,7 @@ test_case_env=$2
 test_case_script=$3
 container_name=$4
 cd ~/sandbox_jobs
-docker run --cpus="0.2" --memory=20m -e ${test_case_env}=${file_name} -v $(pwd)/${file_name}:/bunny_code/${file_name} --name ${container_name} sandbox /bunny_code/${test_case_script}
+docker run --cpus="0.2" --memory=20m -e ${test_case_env}=./${file_name} -v $(pwd)/${file_name}:/bunny_code/${file_name} --name ${container_name} sandbox /bunny_code/${test_case_script}
 docker container inspect ${container_name} -f '{"OOM": {{json .State.OOMKilled}}}'
 docker rm ${container_name} >/dev/null 2>&1
 rm -f ~/sandbox_jobs/${file_name}
